36bbec8d52ff0467291a1d733404b3044138abdf
yuanhunglo <lo.yuanhung@gmail.com>
2024-04-09 12:41:53 -0700

    Added Grove Apartments.

---
+++ check_and_update_grove.py
@@ -0,0 +1,96 @@
+import discord
+from discord.ext import commands
+import shutil
+import pandas as pd
+import json
+import os
+
+def same_jsons(file1_path, file2_path):
+    """Check if 2 json files are the same."""
+    # Read the content of the first JSON file
+    with open(file1_path, 'r') as file1:
+        content1 = file1.read()
+
+    # Read the content of the second JSON file
+    with open(file2_path, 'r') as file2:
+        content2 = file2.read()
+
+    # Parse the JSON content into Python objects
+    data1 = json.loads(content1)
+    data2 = json.loads(content2)
+
+    # Compare the Python objects for equality
+    return data1 == data2
+        
+
+def same_csvs(file1_path, file2_path):
+    df1 = pd.read_csv(file1_path)
+    df2 = pd.read_csv(file2_path)
+    return df1.equals(df2)
+
+
+def copy_file(source_path, destination_path):
+    """Copy source file to destination file."""
+    shutil.copyfile(source_path, destination_path)
+    print(f'File copied from {source_path} to {destination_path}')
+
+
+def send_to_discord(file_path):
+    """Use Discord bot to send results to channel."""
+    
+    file_name, file_format = file_path.split('.')
+    if file_format == 'csv':
+        res = pd.read_csv(file_path)
+    elif file_format == 'json':
+        with open(file_path, 'r') as file:
+            content = file.read()
+        res = json.loads(content)
+
+    # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
+    bot = discord.Client()
+
+    # Event triggered when the bot is ready
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user.name} ({bot.user.id})')
+        channel = bot.get_channel(1213971777655668836) 
+        embed = discord.Embed(
+            title='🌲 GROVE Apartments price update',
+            description='[Check latest prices](https://www.livingatthegroveapts.com/#sightmap-3)',
+            color=discord.Color.blue()
+        )
+        
+        # Add fields to the embed
+        if file_format == 'csv':
+            for idx, _res in res.iterrows(): 
+                value_str = f"**Unit price:** ${_res['price']:,}\n**Available:** {_res['available_on']} ({_res['display_lease_term']}-month lease)"
+                embed.add_field(name=f"🏠 Unit #{_res['unit_number']}", value=value_str, inline=False)
+        elif file_format == 'json':
+            for _res in res: 
+                value_str = f"**Unit price:** ${_res['price']:,}\n**Available:** {_res['available_on']} ({_res['display_lease_term']}-month lease)"
+                embed.add_field(name=f"🏠 Unit #{_res['unit_number']}", value=value_str, inline=False)
+        
+        await channel.send(embed=embed)
+        await bot.close()
+
+    # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+    assert DISCORD_TOKEN is not None, "No DISCORD_TOKEN. You need to set token as environment variable!"
+    bot.run(DISCORD_TOKEN)
+
+
+if __name__ == '__main__':
+    old_csv_path = 'old_prices_peartree.csv'
+    latest_csv_path = 'newest_prices_peartree.csv'
+
+    if not same_csvs(latest_csv_path, old_csv_path):
+        # Send latest result to Discord
+        print("Price update! Send latest info to Discord.")
+        send_to_discord(latest_csv_path)
+
+        # Replace snapshot json
+        print("Replace snapshot with latest file.")
+        copy_file(latest_csv_path, old_csv_path)
+    else: 
+        print('No price change.')
+        
---
+++ fetch_data_grove.py
@@ -0,0 +1,45 @@
+from email import header
+import pandas as pd
+import datetime
+import json
+import requests
+
+
+def send_request(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
+    response = requests.get(url=url, headers=headers)
+    return response
+
+
+def process_response(response):
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Request was successful
+        print('Got a response!')
+        df = pd.DataFrame(response.json()['data']['units'])
+        df = df[['unit_number', 'area', 'price', 'available_on', 'display_lease_term']]
+        df['display_lease_term'] = df['display_lease_term'].str.replace(r'[a-zA-Z\s]', '', regex=True).astype(int)
+        # current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # df['insert_time'] = current_time
+        df.head()
+    else:
+        # Request failed
+        print('Request failed with status:', response.status_code, response.reason)
+    return df
+
+
+def save_json(df):
+    df.to_json('newest_prices_peartree.json', orient='records')
+    print('Done saving JSON.')
+
+def save_csv(df):
+    df.to_csv('newest_prices_peartree.csv', header=True, index=False)
+    print('Done saving CSV.')
+
+
+if __name__ == '__main__':
+    # Peartree API URL
+    URL = 'https://sightmap.com/app/api/v1/4yjp202rwxl/sightmaps/2398'
+    response = send_request(URL)
+    df = process_response(response)
+    save_csv(df)
